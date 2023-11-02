dfc249c4c33cf031f1fdea5450dc342e7566d619
Vlen2R <valentin2romero@gmail.com>
2023-11-02 18:21:19 -0300

    [UPDATE] rename wall_product_price
    rename wall_product_price to website_product_price_us

--- website_product_price_us/__manifest__.py
+++ website_product_price_us/__manifest__.py
@@ -1,10 +1,10 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
-    "name": "Wall: Product Price Website",
+    "name": "Product Price in USD on Website",
     "version": "15.0.1.0.0",
     'category': 'Sales/Sales',
     "depends": ["base", "product"],
-    'description': """Wall: Product Price Website - Products & Pricelists""",
+    'description': """Product Price in USD on Website - Products & Pricelists""",
     "license": "LGPL-3",
     "author": "Valentín Romero, Tecnicanet BA",
     "website": "",
