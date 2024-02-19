e6a6ae53ff8ef6dfb6e6674f3fba04f2e357e673
Sebas <sebatista@yahoo.com>
2024-02-19 17:05:08 -0300

    Update product_pricelist.py
    Se agregan modificaciones propuestas por Luis Trajtenberg.

--- website_product_price_us/models/product_pricelist.py
+++ website_product_price_us/models/product_pricelist.py
@@ -2,14 +2,6 @@ from odoo import _, fields, models  # type: ignore
 
 class PricelistItem(models.Model):
     _inherit = "product.pricelist.item"
-
-    base = fields.Selection([
-        ('list_price', 'Sales Price'),
-        ('standard_price', 'Cost'),
-        ('pricelist', 'Other Pricelist'),
-        ('x_studio_precio_en_us', 'Precio en US')], "Based on",
-        default='list_price', required=True,
-        help='Base price for computation.\n'
-             'Sales Price: The base price will be the Sales Price.\n'
-             'Cost Price : The base price will be the cost price.\n'
-             'Other Pricelist : Computation of the base price based on another Pricelist.')
+    
+    base = fields.Selection(selection_add=[('x_studio_precio_en_us', 'Precio en US')], 
+                            ondelete={'x_studio_precio_en_us': 'set default'})
